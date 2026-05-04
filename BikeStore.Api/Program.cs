8cca896db75edc29f4dbadea65630c7dd480ce03
ravi-vishwakarma-hash <vv5845055@gmail.com>
2026-05-04 08:32:59 +0530

    Add project files.

---
+++ BikeStore.Api/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+
+
+app.MapGet("/health", () =>
+{
+    return new { status = "Healthy" };
+});
+
+app.Run();
+
