894e835eef18c233042dc46862f81760d508cb9c
ravi-vishwakarma-hash <vv5845055@gmail.com>
2026-05-04 09:14:20 +0530

    Implement Radis cache.

---
+++ BikeStore.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using BikeStore.Application.Interface;
+using BikeStore.Infrastructure.Service;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace BikeStore.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configureration)
+        {
+            var redisConnection = configureration["Redis:ConnectionString"];
+
+            services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConnection));
+
+            services.AddScoped<ICacheService, RedisCacheService>();
+
+            return services;
+        }
+    }
+}
---
+++ BikeStore.Infrastructure/Service/RedisCacheService.cs
@@ -0,0 +1,42 @@
+﻿using BikeStore.Application.Interface;
+using StackExchange.Redis;
+using System.Text;
+using System.Text.Json;
+
+namespace BikeStore.Infrastructure.Service
+{
+    public class RedisCacheService : ICacheService
+    {
+        private readonly TimeSpan _defaultExpiration = TimeSpan.FromMinutes(5);
+        private readonly IDatabase _redis;
+
+        public RedisCacheService(IConnectionMultiplexer connection)
+        {
+            _redis = connection.GetDatabase();
+        }
+
+        public async Task<T?> GetAsync<T>(string key)
+        {
+            var value = await _redis.StringGetAsync(key);
+            if (!value.HasValue) 
+                return default;
+
+            using var stream = new MemoryStream(value);
+            return await JsonSerializer.DeserializeAsync<T>(stream);
+        }
+
+        public async Task SetAsync<T>(string key, T value, TimeSpan? expiry = null)
+        {
+            using var stream = new MemoryStream();
+            await JsonSerializer.SerializeAsync(stream, value);
+
+            var bytes = stream.ToArray();
+            await _redis.StringSetAsync(key, bytes, expiry ?? _defaultExpiration);
+        }
+
+        public async Task RemoveAsync(string key)
+        {
+            await _redis.KeyDeleteAsync(key);
+        }
+    }
+}
