3ffd3c82f744f909f08e08bb9814fcbfe4da29c1
ravi-vishwakarma-hash <vv5845055@gmail.com>
2026-05-08 09:27:02 +0530

    Enable SQL Server DbContext, disable Redis cache setup
    Program.cs now registers infrastructure services. Redis cache configuration and services are commented out in DependencyInjection.cs. Added BikeDbContext registration using SQL Server connection string with error handling for missing configuration.

--- BikeStore.Api/Program.cs
+++ BikeStore.Api/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-//builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
--- BikeStore.Infrastructure/DependencyInjection.cs
+++ BikeStore.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using BikeStore.Application.Interface;
+using BikeStore.Infrastructure.Persistence.DbContext;
 using BikeStore.Infrastructure.Service;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StackExchange.Redis;
@@ -16,17 +18,29 @@ namespace BikeStore.Infrastructure
             //    ?? throw new InvalidOperationException("Redis settings missing");
 
 
-            var redisConnection = configureration["Redis:ConnectionString"];
+            //var redisConnection = configureration["Redis:ConnectionString"];
 
-            if (string.IsNullOrWhiteSpace(redisConnection))
+            //if (string.IsNullOrWhiteSpace(redisConnection))
+            //{
+            //    throw new InvalidOperationException(
+            //        "Redis connection string is not configured. Please set 'Redis:ConnectionString'.");
+            //}
+
+            //services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConnection));
+
+            //services.AddScoped<ICacheService, RedisCacheService>();
+
+
+            services.AddDbContext<BikeDbContext>(options =>
             {
-                throw new InvalidOperationException(
-                    "Redis connection string is not configured. Please set 'Redis:ConnectionString'.");
-            }
-
-            services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConnection));
-
-            services.AddScoped<ICacheService, RedisCacheService>();
+                var connectionString = configureration.GetConnectionString("bike_store_db");
+                if (string.IsNullOrWhiteSpace(connectionString))
+                {
+                    throw new InvalidOperationException(
+                        "Database connection string is not configured. Please set 'ConnectionStrings:bike_store_db'.");
+                }
+                options.UseSqlServer(connectionString);
+            });
 
             return services;
         }
