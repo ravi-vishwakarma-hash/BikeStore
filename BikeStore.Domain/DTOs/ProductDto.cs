cab6305e8373cf26aef9f9107841442f62f24959
ravi-vishwakarma-hash <vv5845055@gmail.com>
2026-05-09 09:59:56 +0530

    Add ProductDto and IProducts interface for product handling
    Added ProductDto class to represent product data and IProducts interface with async methods for retrieving products by ID and listing all products. These are placed in the appropriate DTOs and Interfaces namespaces.

---
+++ BikeStore.Domain/DTOs/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeStore.Domain.DTOs
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; } = 0;
+
+    }
+}
---
+++ BikeStore.Domain/Interfaces/IProducts.cs
@@ -0,0 +1,13 @@
+﻿using BikeStore.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeStore.Domain.Interfaces
+{
+    internal interface IProducts
+    {
+        Task<IEnumerable<ProductDto>> GetProductsAsync();
+        Task<ProductDto> GetProductByIdAsync(int id);
+    }
+}
