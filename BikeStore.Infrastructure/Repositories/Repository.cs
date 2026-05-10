f2da374842a0d5596c7d79c13144f9d9bfcf9d1d
ravi-vishwakarma-hash <vv5845055@gmail.com>
2026-05-10 12:45:55 +0530

    Add abstract generic Repository base class
    Introduced an internal abstract Repository<TEntity> class in the BikeStore.Infrastructure.Repositories namespace. This class provides Add, Remove, and Update methods for entities using BikeDbContext, enabling reusable data access logic for entity types.

---
+++ BikeStore.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,28 @@
+﻿using BikeStore.Infrastructure.Persistence.DbContext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeStore.Infrastructure.Repositories
+{
+    internal abstract class Repository<TEntity> (BikeDbContext dbContext)
+         where TEntity : class
+    {
+
+        public void Add(TEntity entity)
+        {
+            dbContext.Set<TEntity>().Add(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            dbContext.Set<TEntity>().Remove(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            dbContext.Set<TEntity>().Update(entity);
+        }
+
+    }
+}
